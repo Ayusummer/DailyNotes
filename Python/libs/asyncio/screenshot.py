3cb7a62b407ee3f09eafa71fa28b4ec0d831def2
Ayusummer <1369661643@qq.com>
2021-06-14 09:52:23 +0800

    Create screenshot.py
    - Python 异步编程示例-无头浏览器截图

---
+++ Python/libs/asyncio/screenshot.py
@@ -0,0 +1,20 @@
+# -*- coding : utf-8 -*-
+# @Time      : 2021/6/14 8:14
+# @Author    : 咸鱼型233
+# @File      : screenshot.py
+# @Software  : PyCharm
+# @Function  : 
+# @ChangeLog :
+import asyncio
+from pyppeteer import launch
+
+
+async def main():
+    browser = await launch()        # 启动浏览器
+    page = await browser.newPage()  # 打开新 Tab
+    await page.goto('http://example.com')           # 访问网址
+    await page.screenshot({'path': 'example.png'})  # 截图
+    await browser.close()           # 关闭浏览器
+
+
+asyncio.run(main())
