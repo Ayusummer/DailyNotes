d773719bf83b9d58c5e5e3d471f95d5aafde7c24
Ayusummer <1369661643@qq.com>
2021-10-19 17:13:11 +0800

    :tangerine: LeetCode [208] 实现 Trie (前缀树)

--- 研究方向/算法/LeetCode/CodeStash/208.实现-trie-前缀树.cpp
+++ 研究方向/算法/LeetCode/CodeStash/208.实现-trie-前缀树.cpp
@@ -7,22 +7,46 @@
 // @lc code=start
 class Trie {
 private:
-    vector<Trie>
+    vector<Trie*> children;
+    bool isEnd;
+
+    /*从当前节点查找 prefix, 如果不能完整的查出则返回空指针, 
+        否则返回最后一个字符所在的结点地址*/ 
+    Trie* searchPrefix(string prefix) {
+        Trie* node = this;
+        for (char ch : prefix) {
+            ch -= 'a';
+            if (node->children[ch] == nullptr) {
+                return nullptr;
+            }
+            node = node->children[ch];
+        }
+        return node;
+    }
+
 public:
-    Trie() {
+    Trie() : children(26), isEnd(false) {}
 
-    }
-    
+    /*从当前节点插入一个 word*/
     void insert(string word) {
-
+        Trie* node = this;
+        for (char ch : word) {
+            ch -= 'a';
+            if (node->children[ch] == nullptr) node->children[ch] = new Trie();
+            node = node->children[ch];
+        }
+        node->isEnd = true;
     }
-    
+
+    /* 判断 word 是否在树种*/
     bool search(string word) {
-
+        Trie* node = this->searchPrefix(word);
+        return node != nullptr && node->isEnd;
     }
-    
-    bool startsWith(string prefix) {
 
+    /*判断树中是否有以prefix为前缀的word*/
+    bool startsWith(string prefix) {
+        return this->searchPrefix(prefix) != nullptr;
     }
 };
 
