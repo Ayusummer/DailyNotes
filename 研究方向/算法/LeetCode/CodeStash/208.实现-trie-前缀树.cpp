2c23f042c49598a4756670acf04f4af96b6ffe10
Ayusummer <1369661643@qq.com>
2021-10-19 16:58:07 +0800

    208. 实现 Trie (前缀树)

---
+++ 研究方向/算法/LeetCode/CodeStash/208.实现-trie-前缀树.cpp
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=208 lang=cpp
+ *
+ * [208] 实现 Trie (前缀树)
+ */
+
+// @lc code=start
+class Trie {
+private:
+    vector<Trie>
+public:
+    Trie() {
+
+    }
+    
+    void insert(string word) {
+
+    }
+    
+    bool search(string word) {
+
+    }
+    
+    bool startsWith(string prefix) {
+
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
+// @lc code=end
+
