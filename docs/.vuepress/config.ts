d2782789162336052b81f4c25e8ebaa9ed29e6a3
咸鱼型233 <ayusummer233@qq.com>
2022-11-08 00:51:23 +0800

    📝 录入网络安全相关文档

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -14,6 +14,7 @@ export default defineUserConfig({
         navbar: [
             // 导航栏
             {text: "通识", link: "/通识/通识.md"},
+            {text: "网络安全", link: "/网络安全/通识.md"},
             { text: "Language", link: "/Language/CPlusPlus/C++.md" },
             { text: "后端", link: "/后端/FastAPI/FastAPI.md" },
             { text: "前端", link: "/前端/通识.md" },
@@ -57,6 +58,85 @@ export default defineUserConfig({
                     text: "VSCode",
                     link: "/通识/VSCode.md",
                 },
+            ],
+            "/网络安全/": [
+                {
+                    text: "通识",
+                    link: "/网络安全/通识.md",
+                },
+                {
+                    text: "随笔",
+                    link: "/网络安全/随笔.md",
+                },
+                {
+                    text: "kali",
+                    link: "/网络安全/kali.md",
+                },
+                {
+                    text: "编码",
+                    link: "/网络安全/编码.md",
+                },
+                {
+                    text: "加密算法",
+                    children: [
+                        {
+                            text: "Hash",
+                            link: "/网络安全/加密算法/Hash.md",
+                        },
+                        {
+                            text: "HMAC",
+                            link: "/网络安全/加密算法/HMAC.md",
+                        },
+                    ],
+                },
+                {
+                    text: "漏扫工具",
+                    link: "/网络安全/漏扫工具/AWVS.md",
+                },
+                {
+                    text: "Web安全",
+                    children: [
+                        {
+                            text: "web安全",
+                            link: "/网络安全/Web安全/web安全.md",
+                        },
+                        {
+                            text: "靶场",
+                            children: [
+                                {
+                                    text: "DVWA",
+                                    link: "/网络安全/Web安全/靶场/DVWA.md",
+                                },
+                                {
+                                    text: "Pikachu",
+                                    link: "/网络安全/Web安全/靶场/Pikachu.md",
+                                },
+                            ]
+                        },
+                        {
+                            text: "漏洞类型",
+                            children: [
+                                {
+                                    text: "暴力破解",
+                                    link: "/网络安全/Web安全/漏洞类型/暴力破解/",
+                                },
+                                {
+                                    text: "XSS",
+                                    link: "/网络安全/Web安全/漏洞类型/XSS/",
+                                },
+                            ]
+                        },
+                        {
+                            text: "渗透测试工具",
+                            children: [
+                                {
+                                    text: "BurpSuite",
+                                    link: "/网络安全/Web安全/渗透测试工具/BurpSuite.md",
+                                },
+                            ]
+                        }
+                    ]
+                }
             ],
             "/Bot/": [
                 {
