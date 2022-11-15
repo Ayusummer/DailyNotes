356302cebe8f7472f20d7acff21093d396573071
咸鱼型233 <ayusummer233@qq.com>
2022-11-16 00:16:39 +0800

    refactor🎨:  清理个人认为已经不再必要的练手代码

--- docs/Language/Python/BigDataPythonBase/DailyHomework/Lesson1_basicOperation/ListTest1.py
+++
@@ -1,4 +0,0 @@
-lst=[89,56,78,-64,92]
-total=sum(lst)
-number=len(lst)
-print("列表的平均值是:",total/number)
--- docs/Language/Python/BigDataPythonBase/DailyHomework/Lesson9_stepWiseTask/achievement_management_system/function_asm/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Time    : 2020/11/8 9:37
-# @Author  : 咸鱼型233
-# @File    : __init__.py.py
-# @Software: PyCharm
