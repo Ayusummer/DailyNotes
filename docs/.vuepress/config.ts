e7d5912f1b6fd485bd635b21dc24a7198b9e3ccf
233Official <ayusummr233@gmail.com>
2023-06-26 10:31:16 +0800

    config🔧: 尝试修复searchpro

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -3,40 +3,39 @@ import theme from "./theme";
 import { searchProPlugin } from "vuepress-plugin-search-pro";
 import { sitemapPlugin } from "vuepress-plugin-sitemap2";
 import { seoPlugin } from "vuepress-plugin-seo2";
-
+import { cut } from "nodejs-jieba";
 
 export default defineUserConfig({
-    lang: "zh-CN",
-    // 站点的标题
-    title: "DailyNotes",
-    // 站点的描述
-    description: "233的日常学习记录",
-    // 站点配置, 设置为 /[仓库名]/
-    base: "/DailyNotes/",
-    
-    // 主题配置
-    theme: hopeTheme({
-        navbar: Navbar,
-        // 侧边栏
-        sidebar: {
-            "/通识/Docker/": [
-                {
-                    text: "Docker",
-                    link: "/通识/Docker/Docker.md",
-                },
-                {
-                    text: "Harbor",
-                    link: "/通识/Docker/Harbor.md",
-                },
-            ],
-        }),
-        sitemapPlugin({
-            // 配置选项
-            hostname: "ayusummer.github.io",
-        }),
-        seoPlugin({
-            // 你的选项
-            hostname: "ayusummer.github.io",
-        }),
-    ],
+  lang: "zh-CN",
+  // 站点的标题
+  title: "DailyNotes",
+  // 站点的描述
+  description: "233的日常学习记录",
+  // 站点配置, 设置为 /[仓库名]/
+  base: "/DailyNotes/",
+
+  plugins: [
+    searchProPlugin({
+      // 索引全部内容
+      indexContent: true,
+      hotReload: true,
+      customFields: [
+        {
+          getter: ({ frontmatter }) => frontmatter.tag as string[],
+          formatter: `Tag: $content`,
+        },
+      ],
+      indexOptions: {
+        tokenize: (text, fieldName) =>
+          fieldName === "id" ? [text] : cut(text, true),
+      },
+    }),
+    sitemapPlugin({
+      // 配置选项
+      hostname: "ayusummer.github.io",
+    }),
+  ],
+
+  // 主题配置
+  theme,
 });
