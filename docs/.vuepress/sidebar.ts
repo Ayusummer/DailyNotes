7c36e31324166eba9815b29fead33367c7b4eb8c
233Official <ayusummr233@gmail.com>
2023-09-01 10:44:11 +0800

    PowershellEmpire上线命令分析

--- docs/.vuepress/sidebar.ts
+++ docs/.vuepress/sidebar.ts
@@ -1,307 +1,324 @@
 import { sidebar } from "vuepress-theme-hope";
 
 let NoteTools = [
-    {
-        text: "VuePress",
-        link: "/NoteTools/VuePress.md",
-    },
-    {
-        text: "Markdown",
-        link: "/NoteTools/Markdown.md",
-    },
-    {
-        text: "Mermaid",
-        link: "/NoteTools/Mermaid.md",
-    },
-    {
-        text: "LaTeX",
-        link: "/NoteTools/LaTex/LateX.md",
-    },
-    {
-        text: "PlantUML",
-        link: "/NoteTools/PlantUML.md",
-    },
-    {
-        text: "VitePress",
-        link: "/NoteTools/VitePress.md",
-    },
+  {
+    text: "VuePress",
+    link: "/NoteTools/VuePress.md",
+  },
+  {
+    text: "Markdown",
+    link: "/NoteTools/Markdown.md",
+  },
+  {
+    text: "Mermaid",
+    link: "/NoteTools/Mermaid.md",
+  },
+  {
+    text: "LaTeX",
+    link: "/NoteTools/LaTex/LateX.md",
+  },
+  {
+    text: "PlantUML",
+    link: "/NoteTools/PlantUML.md",
+  },
+  {
+    text: "VitePress",
+    link: "/NoteTools/VitePress.md",
+  },
 ];
 
 let Docker = [
-    {
-        text: "Docker",
-        link: "/通识/Docker/Docker.md",
-    },
-    {
-        text: "Harbor",
-        link: "/通识/Docker/Harbor.md",
-    },
+  {
+    text: "Docker",
+    link: "/通识/Docker/Docker.md",
+  },
+  {
+    text: "Harbor",
+    link: "/通识/Docker/Harbor.md",
+  },
 ];
 
 /** 加密算法侧边栏 */
 let security_encrypt = [
-    {
-        text: "Hash",
-        link: "/网络安全/加密算法/Hash.md",
-    },
-    {
-        text: "HMAC",
-        link: "/网络安全/加密算法/HMAC.md",
-    },
+  {
+    text: "Hash",
+    link: "/网络安全/加密算法/Hash.md",
+  },
+  {
+    text: "HMAC",
+    link: "/网络安全/加密算法/HMAC.md",
+  },
 ];
 
 /** 网络安全-web安全侧边栏 */
 let security_web = [
-    {
-        text: "web安全",
-        link: "/网络安全/Web安全/",
-    },
-    {
-        text: "漏洞类型",
-        children: [
-            {
-                text: "暴力破解",
-                link: "/网络安全/Web安全/漏洞类型/暴力破解/",
-            },
-            {
-                text: "XSS",
-                link: "/网络安全/Web安全/漏洞类型/XSS/",
-            },
-        ],
-    },
-    {
-        text: "渗透测试工具",
-        children: [
-            {
-                text: "BurpSuite",
-                link: "/网络安全/Web安全/渗透测试工具/BurpSuite.md",
-            },
-        ],
-    },
-    {
-        text: "Web漏洞通用型Python复现脚本编写指南",
-        link: "/网络安全/Web安全/Web漏洞通用型Python复现脚本编写指南/",
-    },
+  {
+    text: "web安全",
+    link: "/网络安全/Web安全/",
+  },
+  {
+    text: "漏洞类型",
+    children: [
+      {
+        text: "暴力破解",
+        link: "/网络安全/Web安全/漏洞类型/暴力破解/",
+      },
+      {
+        text: "XSS",
+        link: "/网络安全/Web安全/漏洞类型/XSS/",
+      },
+    ],
+  },
+  {
+    text: "渗透测试工具",
+    children: [
+      {
+        text: "BurpSuite",
+        link: "/网络安全/Web安全/渗透测试工具/BurpSuite.md",
+      },
+    ],
+  },
+  {
+    text: "Web漏洞通用型Python复现脚本编写指南",
+    link: "/网络安全/Web安全/Web漏洞通用型Python复现脚本编写指南/",
+  },
 ];
 
 /** 网络安全-ATTCK 侧边栏 */
 let security_ATTCK = [
-    {
-        text: "ATTCK",
-        link: "/网络安全/ATTCK/",
-    },
-    {
-        text: "TA0001-InitialAccess",
-        link: "/网络安全/ATTCK/TA0001-InitialAccess.md",
-    },
+  {
+    text: "ATTCK",
+    link: "/网络安全/ATTCK/",
+  },
+  {
+    text: "TA0001-InitialAccess",
+    link: "/网络安全/ATTCK/TA0001-InitialAccess.md",
+  },
 ];
 
 /** language-python */
 let language_python = [
-    {
-        text: "QuickStart",
-        link: "/Language/Python/QuickStart.md",
-    },
-    {
-        text: "开发环境",
-        link: "/Language/Python/开发环境.md",
-    },
-    {
-        text: "Note-python",
-        link: "/Language/Python/Note-python.md",
-    },
-    {
-        text: "PEP8",
-        link: "/Language/Python/PEP8.md",
-    },
-    {
-        text: "PythonWeb",
-        link: "/Language/Python/PythonWeb.md",
-    },
-    {
-        text: "libs",
-        children: [
-            {
-                text: "asyncio",
-                link: "/Language/Python/libs/asyncio/异步编程.md",
-            },
-            {
-                text: "json",
-                link: "/Language/Python/libs/json/json.md",
-            },
-            {
-                text: "Matplotlib",
-                link: "/Language/Python/libs/Matplotlib/Matplotlib.md",
-            },
-            {
-                text: "numpy",
-                link: "/Language/Python/libs/numpy/numpy.md",
-            },
-            {
-                text: "OpenCV-python",
-                link: "/Language/Python/libs/OpenCV/OpenCV-python.md",
-            },
-            {
-                text: "Pandas",
-                link: "/Language/Python/libs/Pandas/Pandas.md",
-            },
-            {
-                text: "Pillow",
-                link: "/Language/Python/libs/Pillow/Pillow.md",
-            },
-            {
-                text: "TensorFlow",
-                link: "/Language/Python/libs/TensorFlow/TensorFlow.md",
-            },
-            {
-                text: "turtle",
-                link: "/Language/Python/libs/turtle/turtle.md",
-            },
-            {
-                text: "Rocketry",
-                link: "/Language/Python/libs/Rocketry/",
-            },
-        ],
-    },
+  {
+    text: "QuickStart",
+    link: "/Language/Python/QuickStart.md",
+  },
+  {
+    text: "开发环境",
+    link: "/Language/Python/开发环境.md",
+  },
+  {
+    text: "Note-python",
+    link: "/Language/Python/Note-python.md",
+  },
+  {
+    text: "PEP8",
+    link: "/Language/Python/PEP8.md",
+  },
+  {
+    text: "PythonWeb",
+    link: "/Language/Python/PythonWeb.md",
+  },
+  {
+    text: "libs",
+    children: [
+      {
+        text: "asyncio",
+        link: "/Language/Python/libs/asyncio/异步编程.md",
+      },
+      {
+        text: "json",
+        link: "/Language/Python/libs/json/json.md",
+      },
+      {
+        text: "Matplotlib",
+        link: "/Language/Python/libs/Matplotlib/Matplotlib.md",
+      },
+      {
+        text: "numpy",
+        link: "/Language/Python/libs/numpy/numpy.md",
+      },
+      {
+        text: "OpenCV-python",
+        link: "/Language/Python/libs/OpenCV/OpenCV-python.md",
+      },
+      {
+        text: "Pandas",
+        link: "/Language/Python/libs/Pandas/Pandas.md",
+      },
+      {
+        text: "Pillow",
+        link: "/Language/Python/libs/Pillow/Pillow.md",
+      },
+      {
+        text: "TensorFlow",
+        link: "/Language/Python/libs/TensorFlow/TensorFlow.md",
+      },
+      {
+        text: "turtle",
+        link: "/Language/Python/libs/turtle/turtle.md",
+      },
+      {
+        text: "Rocketry",
+        link: "/Language/Python/libs/Rocketry/",
+      },
+    ],
+  },
 ];
 
 /** language-java */
 let language_java = [
-    {
-        text: "Java",
-        link: "/Language/Java/Java.md",
-    },
-    {
-        text: "Java代码审计",
-        link: "/Language/Java/Java代码审计.md",
-    },
-    {
-        text: "JavaWeb",
-        link: "/Language/Java/JavaWeb.md",
-    },
+  {
+    text: "Java",
+    link: "/Language/Java/Java.md",
+  },
+  {
+    text: "Java代码审计",
+    link: "/Language/Java/Java代码审计.md",
+  },
+  {
+    text: "JavaWeb",
+    link: "/Language/Java/JavaWeb.md",
+  },
 ];
 
 /** 后端-数据库 */
 let backend_database = [
-    {
-        text: "数据库",
-        link: "/后端/数据库/数据库.md",
-    },
-    {
-        text: "MySQL",
-        link: "/后端/数据库/MySQL.md",
-    },
-    {
-        text: "SQLite",
-        link: "/后端/数据库/SQLite.md",
-    },
-    {
-        text: "Redis",
-        link: "/后端/数据库/Redis.md",
-    },
-    {
-        text: "SQLAlchemy",
-        link: "/后端/数据库/SQLAlchemy.md",
-    },
+  {
+    text: "数据库",
+    link: "/后端/数据库/数据库.md",
+  },
+  {
+    text: "MySQL",
+    link: "/后端/数据库/MySQL.md",
+  },
+  {
+    text: "SQLite",
+    link: "/后端/数据库/SQLite.md",
+  },
+  {
+    text: "Redis",
+    link: "/后端/数据库/Redis.md",
+  },
+  {
+    text: "SQLAlchemy",
+    link: "/后端/数据库/SQLAlchemy.md",
+  },
 ];
 
 /** 前端-Vue */
 let frontend_vue = [
-    {
-        text: "Vue",
-        link: "/前端/VUE/Vue.md",
-    },
-    {
-        text: "Vben",
-        link: "/前端/VUE/Vben.md",
-    },
-    {
-        text: "vue-admin-template",
-        link: "/前端/VUE/vue-admin-template.md",
-    },
+  {
+    text: "Vue",
+    link: "/前端/VUE/Vue.md",
+  },
+  {
+    text: "Vben",
+    link: "/前端/VUE/Vben.md",
+  },
+  {
+    text: "vue-admin-template",
+    link: "/前端/VUE/vue-admin-template.md",
+  },
 ];
 
 /** Bot */
 let Bot = [
-    {
-        text: "Bot",
-        link: "/娱乐/Bot/",
-    },
-    {
-        text: "频道Bot",
-        link: "/娱乐/Bot/频道Bot.md",
-    },
-    {
-        text: "企微Bot",
-        link: "/娱乐/Bot/企微Bot.md",
-    },
-    {
-        text: "云崽",
-        link: "/娱乐/Bot/云崽.md",
-    },
+  {
+    text: "Bot",
+    link: "/娱乐/Bot/",
+  },
+  {
+    text: "频道Bot",
+    link: "/娱乐/Bot/频道Bot.md",
+  },
+  {
+    text: "企微Bot",
+    link: "/娱乐/Bot/企微Bot.md",
+  },
+  {
+    text: "云崽",
+    link: "/娱乐/Bot/云崽.md",
+  },
 ];
 
 /** sovits */
 let sovits = [
-    {
-        text: "sovits_32k",
-        link: "/娱乐/sovits/sovits_32k.md",
-    },
-    {
-        text: "sovits_4",
-        link: "/娱乐/sovits/sovits_4.md",
-    },
+  {
+    text: "sovits_32k",
+    link: "/娱乐/sovits/sovits_32k.md",
+  },
+  {
+    text: "sovits_4",
+    link: "/娱乐/sovits/sovits_4.md",
+  },
 ];
 
 /** 内网渗透 */
 let penetration = [
-    {
-        text: "内网渗透",
-        link: "/网络安全/内网渗透/",
-    },
-    {
-        text: "代理转发",
-        link: "/网络安全/内网渗透/代理转发.md",
-    },
+  {
+    text: "内网渗透",
+    link: "/网络安全/内网渗透/",
+  },
+  {
+    text: "代理转发",
+    link: "/网络安全/内网渗透/代理转发.md",
+  },
+  {
+    text: "PowershellEmpire",
+    link: "/网络安全/内网渗透/Powershell-Empire.md",
+  },
 ];
 
 /** 信息收集 */
 let information_collection = [
-    {
-        text: "端口扫描",
-        link: "/网络安全/信息收集/端口扫描/nmap.md",
-    },
-    {
-        text: "漏扫工具",
-        link: "/网络安全/信息收集/漏扫工具/AWVS.md",
-    },
+  {
+    text: "端口扫描",
+    link: "/网络安全/信息收集/端口扫描/nmap.md",
+  },
+  {
+    text: "漏扫工具",
+    link: "/网络安全/信息收集/漏扫工具/AWVS.md",
+  },
 ];
 
 /** Go */
 let Go = [
-    {
-        text: "Go",
-        link: "/Language/Go/",
-    },
-    {
-        text: "Go语言圣经学习随笔",
-        link: "/Language/Go/Go语言圣经学习随笔.md",
-    },
+  {
+    text: "Go",
+    link: "/Language/Go/",
+  },
+  {
+    text: "Go语言圣经学习随笔",
+    link: "/Language/Go/Go语言圣经学习随笔.md",
+  },
+];
+
+/** Shell */
+let Shell = [
+  {
+    text: "Shell",
+    link: "/Language/Shell/",
+  },
+  {
+    text: "Powershell",
+    link: "/Language/Shell/Powershell/",
+  },
 ];
 
 export const Sidebar = sidebar({
-    "/NoteTools/": NoteTools,
-    "/通识/Docker/": Docker,
-    "/Language/Python/": language_python,
-    "/Language/Java/": language_java,
-    "/后端/数据库/": backend_database,
-    "/前端/VUE/": frontend_vue,
-    "/娱乐/Bot/": Bot,
-    "/娱乐/sovits/": sovits,
-    "/网络安全/加密算法/": security_encrypt,
-    "/网络安全/ATTCK/": security_ATTCK,
-    "/网络安全/Web安全/": security_web,
-    "/网络安全/内网渗透/": penetration,
-    "/网络安全/信息收集/": information_collection,
-    "/Language/Go/": Go,
+  "/NoteTools/": NoteTools,
+  "/通识/Docker/": Docker,
+  "/Language/Python/": language_python,
+  "/Language/Java/": language_java,
+  "/Language/Shell/": Shell,
+  "/Language/Go/": Go,
+  "/后端/数据库/": backend_database,
+  "/前端/VUE/": frontend_vue,
+  "/娱乐/Bot/": Bot,
+  "/娱乐/sovits/": sovits,
+  "/网络安全/加密算法/": security_encrypt,
+  "/网络安全/ATTCK/": security_ATTCK,
+  "/网络安全/Web安全/": security_web,
+  "/网络安全/内网渗透/": penetration,
+  "/网络安全/信息收集/": information_collection,
 });
